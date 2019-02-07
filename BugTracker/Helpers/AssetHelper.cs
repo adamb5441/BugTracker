742189f59eff8e844890ff1661490bf65a057adf
adamb5441 <40124072+adamb5441@users.noreply.github.com>
2019-02-07 13:02:13 -0500

    Icon helper and asset helper finished

---
+++ BugTracker/Helpers/AssetHelper.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.SqlServer.Server;
+using mvcBLOG.Models;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.Configuration;
+
+namespace BugTracker.Helpers
+{
+
+    public static class AssetHelper
+    {
+        public static bool IsWebFriendlyImage(HttpPostedFileBase file)
+        {
+            if (file == null) return false;
+            if (file.ContentLength > 2 * 1024 * 1024 || file.ContentLength < 1024) return false;
+
+            try
+            {
+                foreach(var ext in WebConfigurationManager.AppSettings["ValidExtentions"].Split(','))
+                {
+                    if (Path.GetExtension(file.FileName).Contains(ext))
+                        return true;
+                }
+            }
+            catch
+            {
+                return false;
+            }
+            return false;
+        }
+    }
+}
---
+++ BugTracker/Helpers/IconHelper.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+
+namespace BugTracker.Helpers
+{
+    public class IconHelper
+    {
+        public static string GetIcon(string FileName)
+        {
+            var Icon = "";
+            var fileExtention = Path.GetExtension(FileName);
+            switch (fileExtention)
+            {
+                case ".pdf":
+                    Icon = "fa fa-file-pdf-o";
+                    break;
+                case ".doc":
+                case ".docx":
+                    Icon = "fa fa-file-word-o";
+                    break;
+                case ".xls":
+                    Icon = "fa fa-file-exel=o";
+                    break;
+                case ".txt":
+                    Icon = "fa fa-file-txt-o";
+                    break;
+                case ".png":
+                case ".jpeg":
+                case ".gif":
+                    Icon = "fa fa-file-image-o";
+                    break;
+                default:
+                    Icon = "fa fa-file-code-o";
+                    break;
+            }
+            return Icon;
+        }
+    }
+}
