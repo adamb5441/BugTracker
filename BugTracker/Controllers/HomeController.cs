4c3ee3abdd62befe4f8bb8ec909d4319e5fbeb03
adamb5441 <40124072+adamb5441@users.noreply.github.com>
2019-02-17 20:41:33 -0500

    Notification changed refine notifications and history futher.

--- BugTracker/Controllers/HomeController.cs
+++ BugTracker/Controllers/HomeController.cs
@@ -21,7 +21,7 @@ namespace BugTracker.Controllers
             {
                 var proj = ProjectHelper.ListUserProjects(User.Identity.GetUserId());
                 var userId = User.Identity.GetUserId();
-                var tickets = db.Tickets.Where(b => b.AssignedToUserId == userId || b.OwnerUserId == userId).AsEnumerable<Ticket>(); ;
+                var tickets = db.Tickets.Where(b => b.AssignedToUserId == userId || b.OwnerUserId == userId).AsEnumerable<Ticket>(); 
 
                 var model = new DashboardMod { Projects = proj, Users = db.Users, Tickets = tickets };
                 return View(model);
