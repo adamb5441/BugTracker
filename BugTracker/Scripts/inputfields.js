dbe806ae36218041dd1964a654ce54e8bdaaaffa
adamb5441 <40124072+adamb5441@users.noreply.github.com>
2019-02-26 10:21:00 -0500

    Errors fixed.

---
+++ BugTracker/Scripts/inputfields.js
@@ -0,0 +1,13 @@
+﻿
+$('.dtable').DataTable();
+$(".pastcomments").hide()
+$("#showcom").show()
+$("#showcom").click(() => {
+    $(".pastcomments").toggle()
+    $("#showcom").hide();
+})
+$(".comment").autogrow();
+$("#cancel").click(function (e) {
+    e.preventDefault();
+    $("#txtarea").val("")
+})
